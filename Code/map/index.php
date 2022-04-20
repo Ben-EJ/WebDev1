315ecd7d78bfcc76218950cae02028b731b5e332
Ben <56438486+hellhound858@users.noreply.github.com>
2022-04-20 17:54:40 +0000

    New maps feature added

--- Code/map/index.php
+++ Code/map/index.php
@@ -3,10 +3,10 @@
   <head>
     <title>Heatmaps</title>
     <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+    <link rel="stylesheet" href="/map/maps.css">
   </head>
     <script>
         let map;
-
         //16 elements (Green = 0-5, Orange = 6-10 Red = 11-16)
         function newColourArray(len){
             const colours = ["#ccffcc","#80ff80","#4dff4d","#33ff33","#00cc00","#006600",  "#ffc299", "#ff944d", "#ff6600", "#e65c00", "#b34700",  "#ff8080", "#ff3333", "#ff0000", "#cc0000", "#990000"];
@@ -46,6 +46,40 @@
             console.log(newRed);
             return newGreen.concat(newOrange,newRed);
         }
+        const colourEncodings = ["#9CFF9C","#31FF00","#31CF00",  "#FFFF00","#FFCF00","#FF9A00","#FF6464","#FF0000","#990000","#CE30FF"];
+        function colourLevel(level){
+            if(level <= 67){
+                return colourEncodings[0];
+            }
+            else if(level >= 68 && level <= 134){
+                return colourEncodings[1];
+            }
+            else if(level >= 135 && level <= 200){
+                return colourEncodings[2];
+            }
+            else if(level >= 201 && level <= 267){
+                return colourEncodings[3];
+            }
+            else if(level >= 268 && level <= 334){
+                return colourEncodings[4];
+            }
+            else if(level >= 335 && level <= 400){
+                return colourEncodings[5];
+            }
+            else if(level >= 401 && level <= 467){
+                return colourEncodings[6];
+            }
+            else if(level >= 468 && level <= 534){
+                return colourEncodings[7];
+            }
+            else if(level >= 535 && level <= 600){
+                return colourEncodings[8];
+            }
+            else if(level <= 601){
+                return colourEncodings[9];
+            }
+        }
+
         function generateMarker(map,location, pollutantAmt, colour){
             return new google.maps.Marker({
                 position: location,
@@ -70,7 +104,13 @@
             const dateSplitArray = [splitDate[0],splitDate[1],splitDate[2],splitTime[0], splitTime[1],splitTime[2]];
             return dateSplitArray;
         }
-
+        function emptyMap(){
+            console.log("initStart");
+            map = new google.maps.Map(document.getElementById("map"), {
+                center: { lat: 51.4545, lng: -2.5879 },
+                zoom: 12,
+            });
+        }
         function initMap() {
             console.log("initStart");
             map = new google.maps.Map(document.getElementById("map"), {
@@ -188,47 +228,118 @@
                 }
             });
         }
+
+        function initMapColourEncodings() {
+            console.log("initStart");
+            map = new google.maps.Map(document.getElementById("map"), {
+                center: { lat: 51.4545, lng: -2.5879 },
+                zoom: 12,
+            });
+
+            var selectYear = document.getElementById('year');
+            var valueYear = selectYear.options[selectYear.selectedIndex].value;
+
+            var selectMonth = document.getElementById('month');
+            var valueMonth = selectMonth.options[selectMonth.selectedIndex].value;
+
+            var selectDay = document.getElementById('day');
+            var valueDay = selectDay.options[selectDay.selectedIndex].value;
+
+            var selectTime = document.getElementById('time');
+            var time = selectTime.options[selectTime.selectedIndex].value;
+
+            var selectPollutants  = document.getElementById('pollutant');
+            var pollutant = selectPollutants.options[selectPollutants.selectedIndex].value;
+
+            const promises = [];
+            const dataXML = ["data-188.xml", "data-203.xml", "data-206.xml", "data-209.xml", "data-213.xml", "data-215.xml", "data-228.xml", "data-270.xml", "data-271.xml", "data-375.xml", "data-395.xml", "data-452.xml", "data-447.xml", "data-459.xml", "data-463.xml", "data-500.xml", "data-501.xml"]//"data-481.xml"
+            
+            for(var i = 0; i < dataXML.length; i++){
+                let ajaxPromise = new Promise(function(myResolve, myReject) {
+                    const xhttp = new XMLHttpRequest();
+                    xhttp.onload = function() {
+                        myResolve(this.responseXML);
+                    }
+                    xhttp.onerror = function() {
+                        myReject("error");
+                    }
+                    xhttp.open("GET", "/WebDev1/Code/xmlFiles/" + dataXML[i]);
+                    xhttp.send();
+                });
+                promises.push(ajaxPromise);
+            }
+            Promise.all(promises).then((xmlDocs) => {
+                console.log("Promises Complete");
+                var data = [];
+                for(var i = 0; i < xmlDocs.length; i++){
+                    const station = xmlDocs[i].getElementsByTagName("station");
+                    const recElements = xmlDocs[i].getElementsByTagName("rec");
+                    for(var x = 0; x < recElements.length; x++){
+                        ts = recElements[x].getAttribute('ts');
+                        dateTime = getDate(ts);
+                        if(dateTime[2] == valueYear && dateTime[1] == valueMonth && dateTime[0] == valueDay){
+                            if(dateTime[3].toString().trim() == time.trim()){
+                                var latLonRaw = station[0].getAttribute('geocode');
+                                var array = latLonRaw.split(",");
+                                if(pollutant.trim() == "nox"){
+                                    var thePolNOX = parseInt(recElements[x].getAttribute('nox'));
+                                    if(thePolNOX == null){
+                                        console.log(thePolNOX);
+                                    }else{
+                                        generateMarker(map, { lat: parseFloat(array[0]), lng: parseFloat(array[1]) }, thePolNOX, colourLevel(thePolNOX));
+                                        console.log("Found");
+                                    }
+                                }
+                                else if(pollutant.trim() == "no"){
+                                    var thePolNO = parseInt(recElements[x].getAttribute('no'));
+                                    if(thePolNO == null){
+                                        console.log(thePolNO);
+                                    }else{
+                                        generateMarker(map, { lat: parseFloat(array[0]), lng: parseFloat(array[1]) }, thePolNO, colourLevel(thePolNO));
+                                        console.log("Found");
+                                    }
+                                }
+                                else if(pollutant.trim() == "no2"){
+                                    var thePolNO2 = parseInt(recElements[x].getAttribute('no2'));
+                                    if(thePolNO2 == null){
+                                        console.log(thePolNO2);
+                                    }else{
+                                        generateMarker(map, { lat: parseFloat(array[0]), lng: parseFloat(array[1]) }, thePolNO2, colourLevel(thePolNO2));
+                                        console.log("Found");
+                                    }
+                                }
+                            }                    
+                        }
+                    }
+                }
+
+                var colourTable = document.getElementById("colourTable");
+                colourTable.innerHTML = "";
+                for(let i = 0; i < colourEncodings.length; i++){
+                   var row = colourTable.insertRow();
+                   var cellA = row.insertCell(0);
+                   var cellB = row.insertCell(1);
+                   
+                   cellA.innerHTML = '<div class="colorCell" style="background-color:' +  colourEncodings[i] + ';"></div>';
+                   if(i == 0){
+                        cellB.innerHTML = "0";
+                   }else if(i == colourEncodings.length - 2){
+                        cellB.innerHTML = "v";
+                   }
+                   else if(i == colourEncodings.length - 1){
+                        cellB.innerHTML = "60";
+                   }else{
+                        cellB.innerHTML = "|";
+                   }
+                }
+            });
+        }
+
         window.initMap = initMap;
         
     </script>
   
   <body>
-    <style>
-        html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-        }
-        .MainMapDiv{
-            top:25%;
-            left:25%;
-            height:100%;
-            width:100%;
-            position: absolute;
-        }
-        #map {
-            height:50%;
-            width:50%;
-        }
-        table {
-            border: 1px solid black;
-        }
-        td {
-            width: 10px;
-            text-align: center;
-            padding: 5px;
-        }
-        .ColourTable{
-            top:10%;
-            left:50%;
-            position: absolute;
-        }
-        .colorCell{
-            padding: 15px;
-            height:20%;
-            width:40%;
-        }
-    </style>
     <div class="MainMapDiv">
     <select name="year" id="year">
     <option value="" selected="selected">Select Year</option>
@@ -324,16 +435,14 @@
     <option value="22" selected="selected">22:00</option>
     <option value="23" selected="selected">23:00</option>
     </select>
-    <button onclick="initMap()">Generate Map</button>
-
+    <button onclick="initMap()">Generate Map(Heat Map Markers)</button>
+    <button onclick="initMapColourEncodings()">Generate Map(Colour Encodings)</button>
     
     <div id="map"></div>
     <div class="ColourTable"><table id="colourTable"></table></div>
     </div>
-
-    <script
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBy3DhKi_LhOqHmhS-HEbtw6nO63vt5HDc&v=weekly"
-      defer
-    ></script>
+    <script async
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBy3DhKi_LhOqHmhS-HEbtw6nO63vt5HDc&libraries=visualization&callback=emptyMap">
+    </script>
   </body>
 </html>
---
+++ report.md
@@ -0,0 +1,8 @@
+<h1>Parsing Methods</h1>
+<p></p>
+
+<h1>Enhancing Visualisation</h1>
+<p></p>
+
+<h1>Learning Outcomes Discussion</h1>
+<p></p>
